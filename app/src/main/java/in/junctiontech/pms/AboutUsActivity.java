9f5dc24fd82baf7575d78cfee6b679fd8abe9853
deep <deep.k.sharma@gmail.com>
2015-10-31 13:16:42 +0530

    Check

--- app/src/main/java/in/junctiontech/pms/AboutUsActivity.java
+++ app/src/main/java/in/junctiontech/pms/AboutUsActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class AboutUsActivity extends AppCompatActivity {
 
+    int a;// TODO check
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
